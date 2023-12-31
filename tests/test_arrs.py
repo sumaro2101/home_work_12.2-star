80a98b4099f02fb91a5fc855af2405be99cde307
Alex Pavlov <yukiu217@gmail.com>
2023-12-30 17:22:49 -0800

    make covered full 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,3 +1,4 @@
+import pytest
 from utils import arrs
 
 
@@ -5,7 +6,9 @@ def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
-
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+@pytest.mark.parametrize("coll, start, end, expected_result", [([1, 2, 3, 4], 1, 3, [2, 3]),
+                                                               ([1, 2, 3] , 1, None, [2, 3]),
+                                                               ([], 1, None, []),
+                                                               ([1, 2, 3, 4, 5], None, -2, [1, 2, 3])])
+def test_slice(coll, start, end, expected_result):
+    assert arrs.my_slice(coll, start, end) == expected_result
